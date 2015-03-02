f0974756bc7b9aa5abdd9614cd5c9b17ed27cb49
lc <ziyu0123456789@gmail.com>
2015-03-02 22:49:20 +0800

    正则测试
    测试了简单的子表达式和后项引用的正则表达式, Qt不能支持子表达式重命名,但是支持组名ID的方式来引用

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::on_pushButton_clicked() {
+    QString str = "OTDR-ACTIVE::1:2:PORT=10,POWER=10;OTDR-ACTIVE::1:2:PORT=10,POWER=10;";
+    QRegExp rx("OTDR-ACTIVE::1:2:PORT=(\\d+),POWER=\\1;"); // primitive floating point matching
+
+    QStringList qStringList;
+    int pos = 0;
+    while ((pos = rx.indexIn(str, pos)) != -1) {
+      qStringList << rx.cap(1);
+      pos += rx.matchedLength();
+    }
+
+  qDebug() << qStringList << endl;
 }
